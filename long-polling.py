985e52f8524e7ccf4b3ca3cebb61af89befe16cb
Aditya Yadav <aditya18yadav07@gmail.com>
2025-02-23 12:36:19 +0530

    Update long-polling.py

--- long-polling.py
+++ long-polling.py
@@ -29,18 +29,18 @@ def send_message(chat_id, text):
     requests.post(url, data=data)
 
 """
-Added by AkshitBhandari - 27016
-This function uses and API to fetch an joke from the joke API 
-It basically provides us with a python dictionary that has keys like type, setup and punchline which contains specific string (or we can say the main content or joke)
-This data will be called to show up the joke as I did in line 43 of code
+Added by Aditya Yadav - 27226
+This function uses and API to fetch weather from the weather API 
+It basically provides us with a python dictionary that has keys like type, setup and punchline which contains specific string (or we can say the main content or weather)
+This data will be called to show up the weather as I did in line 43 of code
 """
-def get_joke():
-    joke_url = "https://official-joke-api.appspot.com/jokes/random"
-    response = requests.get(joke_url)
+def weather():
+    weather_url = "https://official-weather-api.appspot.com/weather/random"
+    response = requests.get(weather_url)
     if response.status_code == 200:
-        joke_data = response.json()
-        return f"{joke_data['setup']}\n{joke_data['punchline']}"
-    return "Sorry, I couldn't fetch a joke at the moment."
+        weather_data = response.json()
+        return (f"{weather_data['setup']}\n{weather_data['punchline']}")
+    return "Sorry, I couldn't fetch the weather at the moment."
 
 def main():
     update_id = None
@@ -57,16 +57,16 @@ def main():
             if text == "/start":
                 greeting = random.choice(greetings)
                 send_message(chat_id, greeting)
-            elif text == "/joke":
+            elif text == "/weather":
                 """
-                This block checks if the command /joke is typed by the user while using the bot and helps us to send the joke (refer line 66)
+                This block checks if the command /weather is typed by the user while using the bot and which helps us to send the weather details(refer from line 66)
                 """
-                joke = get_joke()
-                send_message(chat_id, joke)
+                weather = weather()
+                send_message(chat_id, weather)
             else:
                 send_message(chat_id, "Invalid message")
 
-        time.sleep(0.5)
+        time.sleep(0.6)
 
 if __name__ == "__main__":
     polling_thread = threading.Thread(target=main)
